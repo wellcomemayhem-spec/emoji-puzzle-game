9b8baca99918f206b4665b8b61bf94a52e06907c
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-08-16 22:52:48 +0800

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Emoji Puzzle Game</h1>
+                <p>Match the emojis based on visual clues!</p>
+                {/* Game Component will go here */}
+            </header>
+        </div>
+    );
+}
+
+export default App;
